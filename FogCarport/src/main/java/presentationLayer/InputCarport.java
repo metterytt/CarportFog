075eaf408f054e3be2f10c3bd1b45446a3539a7f
Pernille Halberg Lørup <pernillelorup@Pernilles-MacBook-Pro.local>
2018-04-25 10:04:57 +0200

    calculator package

--- FogCarport/src/main/java/presentationLayer/InputCarport.java
+++ FogCarport/src/main/java/presentationLayer/InputCarport.java
@@ -5,8 +5,8 @@
  */
 package presentationLayer;
 
-import Calculators.CarportCalculator;
-import Calculators.FlatNoShedCalculator;
+import functionLayer.Calculator.CarportCalculator;
+import functionLayer.Calculator.FlatNoShedCalculator;
 import functionLayer.BOM;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
