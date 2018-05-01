80b8762fe6038b7bcce7d3bebf8cc589c38fcc2d
metterytt <metterytt@live.dk>
2018-05-01 10:28:16 +0200

    beregninger

--- FogCarport/src/main/java/functionLayer/BOM.java
+++ FogCarport/src/main/java/functionLayer/BOM.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package functionLayer;
 
 import functionLayer.entity.LineItem;
 import java.util.ArrayList;
 
-/**
- *
- * @author mette
- */
 public class BOM {
 
     private ArrayList<LineItem> listOfProducts = new ArrayList<>();
